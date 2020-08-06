2e68de907803b75555dded3e318be6175d213bd2
aflextr <53778891+aflextr@users.noreply.github.com>
2020-08-06 20:57:04 +0300

    Update myapi.py

--- myapi.py
+++ myapi.py
@@ -1,14 +1,15 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+from torrequest import TorRequest
 
-
+    
+            
 url = "https://www.my-api.co/proxy.php"
 
 sayac =0
 
 sayi = int(input("kac tane cekilsin:"))
-
 for i in range(0,sayi):
     link = requests.get(url)
     Soup = BeautifulSoup(link.content,"html.parser")
@@ -20,8 +21,18 @@ for i in range(0,sayi):
         dosya.writelines(Soup)
         dosya.write("\n")
         dosya.close()
-        os.system('cd src && bash regex.sh')    
+        os.system('cd src && bash regex.sh')
+    if sayac >= 5:
+        with TorRequest() as tr:
+            response = tr.get('https://www.my-api.co/proxy.php')
+        Soup1 = BeautifulSoup(response.content,"html.parser")
+        print(Soup1)
+        print("proxy :",sayac)
+        dosya = open("src/regex.txt","a")
+        dosya.writelines(Soup1)
+        dosya.write("\n")
+        dosya.close()
+        os.system('cd src && bash regex.sh')
+    
 
-
-
-
+        
